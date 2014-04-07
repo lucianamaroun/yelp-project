095ecbf5fa64483e8c56aff51b4d031730bc3d72
Luciana Maroun <lubm@phoenix.lbd.dcc.ufmg.br>
2014-04-07 13:29:49 -0300

    Basic dataset statistics.

---
+++ graph.py
@@ -0,0 +1,106 @@
+""" Friendship graph statistics of the Yelp dataset """
+
+import networkx as nx
+import json
+import pickle
+import os
+
+
+USER_JSON = 'data/yelp_academic_dataset_user.json'
+USER_PKL = 'user_graph.pkl'
+
+
+def paths_length_two(graph):
+  """ Counts the number of paths with length two in the graph.
+
+  Args:
+    graph: the networkx.Graph() object.
+
+  Returns:
+    An integer representing the number of length two paths.
+  """
+  count = 0
+  for node1 in graph.nodes_iter():
+    for node2 in graph[node1]: 
+      count += len([node for node in graph[node2] if node > node1]) 
+        # the lexicographical order is to avoid repetition
+  return count 
+
+
+def average_degree(graph):
+  """ Calculates the average degree of the graph.
+
+  Args:
+    graph: the networkx.Graph() object.
+
+  Returns:
+    A float representing average degree.
+  """
+  return sum([graph.degree(node) for node in graph.nodes_iter()]) / \
+    float(graph.number_of_nodes())
+
+
+def load_graph():
+  """ Loads the users graph from the users' json file.
+
+  Observations:
+    - The users json file is a static parameter of the algorithm, defined in the
+      top of this file.
+    - The nodes' id are interpreted as strings.
+
+  Args:
+    None.
+
+  Returns:
+    A networkx.Graph() object.
+  """
+  user_str = open(USER_JSON, 'r').read()
+  users = json.loads(user_str)
+
+  if os.path.exists(USER_PKL):
+    pkl_file = open(USER_PKL, 'r')
+    graph = pickle.load(pkl_file)
+    pkl_file.close()
+  else:
+    graph = nx.Graph()
+    for user in users:
+      graph.add_node(user['user_id'])
+      for friend in user['friends']:
+        graph.add_edge(user['user_id'], friend)
+    pkl_file = open(USER_PKL, 'w')
+    pickle.dump(graph, pkl_file)
+    pkl_file.close()
+
+  return graph
+
+
+def print_statistics():
+  """ Prints in the standard output basic statistics of the graph.
+
+  Args:
+    graph: the networkx.Graph() object.
+
+  Returns:
+    None.
+  """
+  graph = load_graph()
+  n_nodes = graph.number_of_nodes()
+  n_edges = graph.number_of_edges()
+  total_triangles = sum(nx.triangles(graph).values()) / 3
+  components = nx.connected_component_subgraphs(graph)
+  lcc = components[0]
+  print '# nodes: %d' % n_nodes
+  print '# non-isolated nodes: %d' % len([node for node in graph.nodes_iter() if
+    graph.degree(node) > 0])
+  print '# edges: %d' % n_edges
+  print 'clustering coefficient: %.2f' % nx.average_clustering(graph)
+  print 'average degree: %.2f' % average_degree(graph)
+  print '%% nodes in LCC: %.2f%%' % (lcc.number_of_nodes() / float(n_nodes) * 100)
+  print '# components: %d' % len(components)
+  print '# triangles: %d' % total_triangles
+  print '%% closed triangles: %.2f%%' % (float(total_triangles) / \
+    paths_length_two(graph) * 100) 
+
+
+if __name__ == '__main__':
+  print_statistics()
---
+++ statistics.py
@@ -0,0 +1,55 @@
+""" Script with basic statistics of the dataset """
+
+import json
+
+# Number of businesses
+old_busin_str = open('old_data/yelp_academic_dataset_business.json', 'r').read()
+old_busin = json.loads(old_busin_str)
+print '# old business: %d' % len(old_busin)
+
+new_busin_str = open('data/yelp_academic_dataset_business.json', 'r').read()
+new_busin = json.loads(new_busin_str)
+print '# new business: %d' % len(new_busin)
+
+old_busin_ids = set([busin['business_id'] for busin in old_busin])
+new_busin_ids = set([busin['business_id'] for busin in new_busin])
+
+only_old_busin = old_busin_ids.difference(new_busin_ids)
+print '# only old business: %d' % len(only_old_busin)
+
+only_new_busin = new_busin_ids.difference(old_busin_ids)
+print '# only new business: %d' % len(only_new_busin)
+
+# Number of cities
+old_cities = set([busin['city'].lower().strip() for busin in old_busin])
+print '# old cities: %d ' % len(old_cities)
+print old_cities
+
+new_cities = set([busin['city'].lower().strip() for busin in new_busin])
+print '# new cities: %d ' % len(new_cities)
+print new_cities
+
+only_old_cities = old_cities.difference(new_cities)
+print 'only old: ' + str(only_old_cities)
+
+only_new_cities = new_cities.difference(old_cities)
+print 'only new: ' + str(only_new_cities)
+
+# Number of users
+old_user_str = open('old_data/yelp_academic_dataset_user.json', 'r').read()
+old_user = json.loads(old_user_str)
+print '# old users: %d' % len(old_user)
+
+new_user_str = open('data/yelp_academic_dataset_user.json', 'r').read()
+new_user = json.loads(new_user_str)
+print '# new users: %d' % len(new_user)
+
+old_user_ids = set([user['user_id'] for user in old_user])
+new_user_ids = set([user['user_id'] for user in new_user])
+
+only_old_user = old_user_ids.difference(new_user_ids)
+print '# only old user: %d' % len(only_old_user)
+
+only_new_user = new_user_ids.difference(old_user_ids)
+print '# only new user: %d' % len(only_new_user)
+
