3abd8d8600922a04c111b8c7882cc93c0fd37087
Luciana Maroun <lubm@phoenix.lbd.dcc.ufmg.br>
2014-05-06 16:55:50 -0300

    correlation between centrality and usefulness

--- metric-correlation.py
+++ metric-correlation.py
@@ -3,7 +3,9 @@
 """
 
 import networkx
-import scipy
+import scipy.stats
+import pickle
+import os
 
 import graph
 import read
@@ -14,14 +16,19 @@ def calculate_usefulness(reviews):
   for review in reviews:
     if review['user_id'] not in useful:
       useful[review['user_id']] = 0
-    useful[review['user_id']] += reviews['votes']['useful']
+    useful[review['user_id']] += review['votes']['useful']
   return useful
 
 
 def calculate_metric(graph):
   metric = {}
+  if os.path.exists('closeness.pkl'):
+    dump = open('closeness.pkl', 'r')
+    return pickle.load(dump)
   for node in graph:
     metric[node] = networkx.closeness_centrality(graph, node)
+  dump = open('closeness.pkl', 'w')
+  pickle.dump(metric, dump)
   return metric
 
 
@@ -29,20 +36,26 @@ def get_vectors(useful, metric):
   useful_vec = []
   metric_vec = []
   mapping = []
-  for node in graph:
+  for node in useful:
     mapping.append(node)
     useful_vec.append(useful[node])
     metric_vec.append(metric[node])
-  return mapping, useful_vec, metric_vec    
+  return mapping, useful_vec, metric_vec 
 
 
 def calculate_correlation():
+  print 'Reading reviews'
   reviews = read.get_reviews()
+  print 'Calculating usefulness'
   useful = calculate_usefulness(reviews)
-  graph = graph.load_graph()
-  metric = calculate_metric(graph)
+  print 'Reading graph'
+  reviewers_graph = graph.load_graph()
+  print 'Calculating centrality'
+  metric = calculate_metric(reviewers_graph)
+  print 'Getting vectors'
   mapping, u_vec, m_vec = get_vectors(useful, metric)
-  return scipy.stats.stats.pearsonr(u_vec, m_vec)
+  print 'Calculating correlation'
+  return scipy.stats.pearsonr(u_vec, m_vec)
 
 
 if __name__ == '__main__':
