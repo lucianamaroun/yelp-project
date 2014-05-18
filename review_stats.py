7c881e36388efea6c7f52a1065166bf1322d6af9
Izabela Maffra <izabelakarennina@gmail.com>
2014-05-18 15:28:49 -0300

    Verified some timing in statistics

--- review_stats.py
+++ review_stats.py
@@ -1,4 +1,5 @@
 """More statistics of the database."""
+from datetime import datetime
 from matplotlib import pyplot
 from numpy import arange
 
@@ -17,26 +18,57 @@ def barplot(labels, data):
     pyplot.bar(pos, data)
     pyplot.show()
 
+def getdatetime(date):
+    date_split = date.split('-')
+    dt = datetime(int(date_split[0]), int(date_split[1]), int(date_split[2]))
+    return dt
+
 
 # Number of reviews
 reviews_str = open('data/yelp_academic_dataset_review.json', 'r').read()
 reviews = json.loads(reviews_str)
-print len(reviews)
+#print len(reviews)
+
+dates = []
+
+for rev in reviews:
+    date = rev['date']
+    dates.append(getdatetime(date))
+
+
+#earliest = min(dates)
+#latest = max(dates)
+#print earliest.strftime("%d %B %Y")
+#print latest.strftime("%d %B %Y")
+
+#import sys
+#sys.exit()
 
 # What makes a review useful?
 
 # Number of stars?
 stars = []
 stars_hist = {1:0, 2:0, 3:0, 4:0, 5:0}
-acc_votes = {1:0, 2:0, 3:0, 4:0, 5:0}
+acc_votes_useful = {1:0, 2:0, 3:0, 4:0, 5:0}
+acc_votes_cool = {1:0, 2:0, 3:0, 4:0, 5:0}
+acc_votes_funny = {1:0, 2:0, 3:0, 4:0, 5:0}
 length = []
 num_votes = []
+too_new_delta = datetime.timedelta(days=365)
 for rev in reviews:
+    date = rev['date']
+    dt = getdatetime(date)
+    if dt > threshold_dt:
+        continue
     num_stars = rev['stars']
     stars_hist[num_stars] += 1
     stars.append(num_stars)
     useful = rev['votes']['useful']
-    acc_votes[num_stars] += useful
+    cool = rev['votes']['cool']
+    funny = rev['votes']['funny']
+    acc_votes_useful[num_stars] += useful
+    acc_votes_cool[num_stars] += cool
+    acc_votes_funny[num_stars] += funny
     length.append(len(rev['text']))
     num_votes.append(useful)
 #print acc
@@ -44,8 +76,18 @@ for rev in reviews:
 print 'Average number of useful votes per number of stars of review'
 for i in range(1, 6):
     print '%d: %f votes for useful on average.' % (i,
-            acc_votes[i]/float(stars_hist[i]))
+            acc_votes_useful[i]/float(stars_hist[i]))
 
-print 'Analyzing length'
-scatterplot(length, num_votes)
+print 'Average number of cool votes per number of stars of review'
+for i in range(1, 6):
+    print '%d: %f votes for cool on average.' % (i,
+            acc_votes_cool[i]/float(stars_hist[i]))
+
+print 'Average number of funny votes per number of stars of review'
+for i in range(1, 6):
+    print '%d: %f votes for funny on average.' % (i,
+            acc_votes_funny[i]/float(stars_hist[i]))
+
+#print 'Analyzing length'
+#scatterplot(length, num_votes)
 
