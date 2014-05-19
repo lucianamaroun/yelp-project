6cddd653b0db8577bef655d855b5b0f24e9f5bc2
Izabela Maffra <izabelakarennina@gmail.com>
2014-05-18 21:36:12 -0300

    Integrated create_matrix file in collab_filtering file. But sth is broken.

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,6 +1,9 @@
 """A module to perform the collaborative filtering."""
 
-import numpy
+import numpy as np
+import read_json_data as data
+import scipy.optimize as opt
+
 
 def get_optimization_function(ratings, attributes):
   """ Get the function for optimization of the content based recommendation.
@@ -18,7 +21,7 @@ def get_optimization_function(ratings, attributes):
   Returns:
     A functions that evaluates the optimization function.
   """
-  attributes = numpy.column_stack((numpy.ones(attributes.shape[0]), attributes))
+  attributes = np.column_stack((np.ones(attributes.shape[0]), attributes))
   def opt_function(big_theta):
     value = 0
     for i in range(ratings.shape[0]): # restaurant
@@ -30,3 +33,52 @@ def get_optimization_function(ratings, attributes):
       value += sum([(theta ** 2)[0,0] for theta in big_theta[:,j]])
     return value
   return opt_function
+
+def create_index_dict(my_list, id_string):
+  return {x[id_string]:ind for ind, x in enumerate(my_list)}
+
+
+def create_rating_matrix(reviews, users_dict, buss_dict):
+  matrix = np.matrix(np.zeros((len(buss_dict), len(users_dict))))
+  for r in reviews:
+    user_id = r['user_id']
+    buss_id = r['business_id']
+    user_index = users_dict[user_id]
+    buss_index = buss_dict[buss_id]
+    matrix[buss_index, user_index] = r['stars']
+  return matrix
+
+def create_attrs_matrix(businesses, buss_dict):
+  matrix = np.matrix(np.zeros((len(businesses), 4)))
+  for b in businesses:
+    b_index = buss_dict[b['business_id']]
+    matrix[b_index, 0] = b['attributes']['Price Range']
+    matrix[b_index, 1] = b['attributes']['Good For']['lunch']
+    matrix[b_index, 2] = b['attributes']['Good For']['dinner']
+    matrix[b_index, 3] = b['attributes']['Good For']['latenight']
+  return matrix
+ 
+
+def main():
+  users = data.read_users_reduced()
+  businesses = data.read_businesses_reduced()
+  reviews = data.read_reviews_reduced()
+
+  users_dict = create_index_dict(users, 'user_id')
+  buss_dict = create_index_dict(businesses, 'business_id')
+
+  rating_matrix = create_rating_matrix(reviews, users_dict, buss_dict)
+  attrs_matrix = create_attrs_matrix(businesses, buss_dict)
+
+  opt_function = get_optimization_function(rating_matrix, attrs_matrix)
+
+  initial_guess = np.ndarray(shape=(5, len(users)), buffer=np.zeros(5*len(users)))
+  initial_guess[0] = 1
+  #print initial_guess
+
+  opt.minimize(opt_function, initial_guess)
+
+
+
+if __name__ == '__main__':
+  main()
