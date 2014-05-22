ab2924ff4146d6c4a7d500fb38b2154ad770c3de
Luciana Maroun <lucianamaroun@gmail.com>
2014-05-21 22:34:44 -0300

    Script for plotting users and restaurants using k-means for clustering.

---
+++ plots/plot_variables.R
@@ -0,0 +1,11 @@
+# UPDATE FILE LOCATION
+user <- read.csv('/Users/luciana/yelp-project/theta.txt', header=F)
+rest <- read.csv('/Users/luciana/yelp-project/attrs.txt', header=F)
+u_cluster <- kmeans(user, 2)$cluster
+r_cluster <- kmeans(rest, 2)$cluster
+lim <- c(min(c(min(user[,1]), min(user[,2]), min(rest[,1]), min(rest[,2]))), max(c(max(user[,1]), max(user[,2]), max(rest[,1]), max(rest[,2]))))
+plot(1, type='n', xlim=lim, ylim=lim, xlab='Attribute 1', ylab='Attribute 2')
+points(user[u_cluster==1,1], user[u_cluster==1,2], col='red')
+points(user[u_cluster==2,1], user[u_cluster==2,2], col='blue')
+points(rest[r_cluster==1,1], rest[r_cluster==1,2], pch=2, col='green')
+points(rest[r_cluster==2,1], rest[r_cluster==2,2], pch=2, col='purple')
